1a20f8548cc10a579904fcec8bc382bc61b6d897
kmd192kmd <kmd192kmd@gmail.com>
2026-04-10 17:34:34 +0900

    26-04-10 수업 업데이트

---
+++ AjaxTest/src/main/java/com/test/ajax/model/SeqDto.java
@@ -0,0 +1,21 @@
+package com.test.ajax.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class SeqDto {
+
+	private String[] slist;
+	
+}
+
+
+
+
+
+
+
